811298f14ac5d78a5a97f1dbf687ba0be5e9e0ac
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2025-11-19 10:42:14 +0300

    Mango connected

--- 7-2-restful-api/server/db.js
+++ 7-2-restful-api/server/db.js
@@ -8,6 +8,11 @@
 import mongoose from "mongoose";
 
 export async function connectDB(url) {
-  await mongoose.connect(url);
-  console.log("[DB] Mongo connected");
+  try{
+    await mongoose.connect(url);
+    console.log("Mongo connected");
+  }
+  catch(err){
+    console.error("Connection error:", err.message)
+  }
 }
